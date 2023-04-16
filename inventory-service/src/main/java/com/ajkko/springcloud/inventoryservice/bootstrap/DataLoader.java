0383d9502b04ec4a7df691b6682c8f3e24d93c00
Sergii <sergii.khylko@outlook.com>
2023-04-17 00:30:02 +0300

    #8 add module structure

--- inventory-service/src/main/java/com/ajkko/springcloud/inventoryservice/bootstrap/DataLoader.java
+++ inventory-service/src/main/java/com/ajkko/springcloud/inventoryservice/bootstrap/DataLoader.java
@@ -5,6 +5,7 @@ import com.ajkko.springcloud.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -13,11 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final InventoryRepository inventoryRepository;
 
     @Override
+    @Transactional
     public void run(String... args) {
-//        var iPhone13 = Inventory.builder().skuCode("iphone_13").quantity(100).build();
-//        var iPhone13red = Inventory.builder().skuCode("iphone_13_red").quantity(0).build();
-//
-//        inventoryRepository.save(iPhone13);
-//        inventoryRepository.save(iPhone13red);
+        if (inventoryRepository.count() > 0) {
+            return;
+        }
+
+        var iPhone13 = Inventory.builder().skuCode("iphone_13").quantity(100).build();
+        var iPhone13red = Inventory.builder().skuCode("iphone_13_red").quantity(0).build();
+        inventoryRepository.save(iPhone13);
+        inventoryRepository.save(iPhone13red);
     }
 }
--- order-service/src/main/java/com/ajkko/springcloud/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/ajkko/springcloud/orderservice/repository/OrderRepository.java
@@ -3,7 +3,5 @@ package com.ajkko.springcloud.orderservice.repository;
 import com.ajkko.springcloud.orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
